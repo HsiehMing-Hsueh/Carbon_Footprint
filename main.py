f46b3d4a8a7a25fb92e365e7c216c468c7f6b8bb
= <gjerry40105@gmail.com>
2023-04-20 21:29:02 +0800

    ADD_TREEVIEW

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 import tkinter as tk
 from tkinter import ttk
 from datasoerce import CarbonFootPrint
+from PIL import Image, ImageTk
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        
+        #建立topFrame
+        topFrame = ttk.Labelframe(self)
+        topFrame.pack()
+        # ----------------建立logo--------------------
+        logoImage = Image.open('./Images/co2logo.png')
+        resizeImage = logoImage.resize((89,82), Image.LANCZOS)
+        self.logoTkimage = ImageTk.PhotoImage(resizeImage)
+        logoLabel = ttk.Label(topFrame, image=self.logoTkimage)
+        logoLabel.pack(pady=(0,50),side=tk.LEFT)
+        #建立medianFrame
+        medianFrame =ttk.Labelframe(self)
+        medianFrame.pack(side=tk.LEFT)
+        #建立bottomFrame
+        bottomFrame = ttk.Labelframe(self)
+        bottomFrame.pack(side=tk.LEFT)
+        #建立treeViewFrame
+        treeViewFrame = ttk.Labelframe(self)
+        treeViewFrame.pack(side=tk.RIGHT)
+        #建立treeview
+        columns = ('#1', '#2', '#3')
+        tree = ttk.Treeview(treeViewFrame, columns=columns, show='headings')
+        tree.heading('#1', text='country')
+        tree.column("#1", minwidth=0, width=75)
+        tree.heading('#2', text='population')
+        tree.column("#2", minwidth=0, width=100)
+        tree.heading('#3', text='co2')
+        tree.column("#3", minwidth=0, width=30)
+        tree.pack(side=tk.RIGHT)
 
 #主程式
 def main():
@@ -21,7 +49,7 @@ def main():
     
     
     window = Window()
-    window.title("碳足跡CarbonFootPrint改一次title")
+    window.title("碳足跡CarbonFootPrint")
     window.mainloop()
 #啟動點
 if __name__ == "__main__":
