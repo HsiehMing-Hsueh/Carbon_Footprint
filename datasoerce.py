a7102bb7d5a40c9c07fae99878cb8cd720c023d7
謝明學 <gjerry40105@gmail.com>
2023-04-20 16:34:00 +0800

    ADD_DATASOERCE

--- datasoerce.py
+++ datasoerce.py
@@ -5,7 +5,6 @@ from io import StringIO
 
 class CarbonFootPrint():
     
-    
     @classmethod
     def download_aqi(cls) ->list:
         response = requests.get('https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv')
@@ -16,6 +15,7 @@ class CarbonFootPrint():
             next(csvReader)
             
             for item in csvReader:
-                print(item[0],item[1],item[3],item[7],item[19],item[37],item[47],item[70])
+                print(item[0],item[1],item[3],item[7],item[19],item[37],item[43],item[47],item[70])
+                #item[0]=country,item[1]=year,item[3]=population,item[7]=co2,item[19]=co2_per_gdp,item[37]=flaring_co2,item[43],item[47],item[70]
         else:
             raise Exception("下載失敗")
